02ecfcb24d7497506cf86b652d2750c57e50e46f
Pichistelrooy <picho_xeneize@hotmail.com>
2019-09-08 22:43:15 -0300

    commit final

--- TP6/js/functions.js
+++ TP6/js/functions.js
@@ -134,31 +134,4 @@ function buildHtmlTable() {
         tr.appendChild(td);   
         c++;
     })
-    
-    formatearTabla();
-}
-
-function formatearTabla(){
-    $( ".nav" ).remove();
-    $('#clientes').after('<div class="nav" id="nav"></div>');
-    var rowsShown = 20;
-    var rowsTotal = $('#clientes tbody tr').length;
-    var numPages = rowsTotal/rowsShown;
-    for(i = 0;i < numPages;i++) {
-        var pageNum = i + 1;
-        $('#nav').append('<a class="btn btn-primary btn-xs" href="#" rel="'+i+'">'+pageNum+'</a> ');
-    }
-    $('#clientes tbody tr').hide();
-    $('#clientes tbody tr').slice(0, rowsShown).show();
-    $('#nav a:first').addClass('active');
-    $('#nav a').bind('click', function(){
-
-        $('#nav a').removeClass('active');
-        $(this).addClass('active');
-        var currPage = $(this).attr('rel');
-        var startItem = currPage * rowsShown;
-        var endItem = startItem + rowsShown;
-        $('#clientes tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-        css('display','table-row').animate({opacity:1}, 300);
-    });
 }
