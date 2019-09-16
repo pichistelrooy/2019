74a55ccc5bf12232aaaa871c27db0886d59647a0
Pichistelrooy <picho_xeneize@hotmail.com>
2019-09-16 07:14:58 -0300

    commit 1 angular

--- angular/carrito/src/app/app-routing.module.ts
+++ angular/carrito/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from 'src/app/components/product-list/product-list.component';
+import { ProductAddComponent } from 'src/app/components/product-add/product-add.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-add', component: ProductAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/carrito/src/app/app.module.ts
+++ angular/carrito/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { ProductChartComponent } from './components/product-chart/product-chart.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductModifyComponent } from './components/product-modify/product-modify.component';
+import { ProductRemoveComponent } from './components/product-remove/product-remove.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    ProductListComponent,
+    NavbarComponent,
+    FooterComponent,
+    ChartComponent,
+    ProductChartComponent,
+    ProductAddComponent,
+    ProductModifyComponent,
+    ProductRemoveComponent
   ],
   imports: [
     BrowserModule,
