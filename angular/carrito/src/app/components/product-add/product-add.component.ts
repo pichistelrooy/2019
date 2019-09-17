a570a2a9f0e086c475df9ffe39e0da85748947dd
Pichistelrooy <picho_xeneize@hotmail.com>
2019-09-17 20:41:06 -0300

    commit

--- angular/carrito/src/app/components/product-add/product-add.component.ts
+++ angular/carrito/src/app/components/product-add/product-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductListComponent} from 'src/app/components/product-list/product-list.component';
-import { Producto } from 'src/app/models/producto';
+/*import { ProductListComponent} from 'src/app/components/product-list/product-list.component';
+import { Producto } from 'src/app/models/producto';*/
 
 @Component({
   selector: 'app-product-add',
@@ -12,7 +12,7 @@ export class ProductAddComponent implements OnInit {
   constructor() {
   }
 
-  public Guardar(id: number,name: string,description: string,price: number,stock: number){
+  /*public Guardar(id: number,name: string,description: string,price: number,stock: number){
     let lista = new ProductListComponent;
     let producto = new Producto();
     producto.id = id;
@@ -26,11 +26,11 @@ export class ProductAddComponent implements OnInit {
     console.log(description);
     console.log(price);
     console.log(stock);
-  }
+  }*/
 
-  public Hola(a: string){
+  /*public Hola(a: string){
     console.log(a);
-  }
+  }*/
 
   ngOnInit() {
   }
---
+++ repaso/1/1.js
@@ -0,0 +1,25 @@
+var lista = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+
+function mostrar(){
+    console.log(lista);
+}
+
+function borrar(){
+    auxarray = lista;
+    var i;
+    var a;
+    auxarray.forEach(element => {
+        i = 0;   
+        a = -1; 
+        lista.forEach(element2 => {
+            a++;
+            if (element == element2){
+                i++;
+                if (i > 1){
+                    lista.splice(a,1);
+                }
+            }    
+        });
+    });
+    console.log(lista);
+}
