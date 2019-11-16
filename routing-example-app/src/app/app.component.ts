1669d7eae3fb047e2f3922f163a337329ac1532d
Pichistelrooy <picho_xeneize@hotmail.com>
2019-11-16 15:04:22 -0300

    eliminando comentarios y logs

--- routing-example-app/src/app/app.component.ts
+++ routing-example-app/src/app/app.component.ts
@@ -7,26 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing-service';
-  /*private token: string;
-
-  constructor(private UserAsyncService: UserAsyncService) { }
-
-  ngOnInit() {
-    console.log(this.token);
-    this.UserAsyncService.emitEvent
-    .subscribe(
-      res =>
-      {
-        this.token = res;
-        console.log(this.token);
-      }
-    );
-  }
-
-  tokenR(){
-    console.log(this.token);
-    this.token = this.UserAsyncService.token;
-    console.log(this.token);
-  }*/
 }
 
--- routing-example-app/src/app/auth/auth.guard.ts
+++ routing-example-app/src/app/auth/auth.guard.ts
@@ -16,16 +16,13 @@ export class AuthGuard implements CanActivate {
   }
 
   checkLogin(url : string) : boolean {
-    //console.log('Esta logueado: ' + this.userService.token);
     if(this.userService.token === undefined){
       this.userService.token = localStorage.getItem('token');
     }
     if(this.userService.token) { 
-      //console.log('token true ' + this.userService.token);
       return true 
     }
     this.userService.redirectUrl = url;
-    //console.log('token false ' + this.userService.token);
     this.route.navigate(['/login']);
     return false;
   }
